4a04293b491a8ae1e68f51e3019f27a6605b8418
Luke Crimi <lukecrimi15@gmail.com>
2025-05-01 10:49:07 -0400

    Removed unused import

--- App/TkinterFrames.py
+++ App/TkinterFrames.py
@@ -5,7 +5,7 @@ from ttkbootstrap.scrolled import ScrolledText
 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import confusion_matrix
 
 import pandas as pd
 from pandas.api.types import is_numeric_dtype
